cd8be26c6f3c5301b38c181c74a9ab05c7624502
Михаил Миноцкий <smoke-m@yandex.com>
2023-06-26 18:47:35 +0300

    day-4.04

--- backend/tags/serializers.py
+++ backend/tags/serializers.py
@@ -1,9 +1,23 @@
+import webcolors
 from rest_framework import serializers
 
 from .models import Tag
 
 
+class Hex2NameColor(serializers.Field):
+    def to_representation(self, value):
+        return value
+
+    def to_internal_value(self, data):
+        try:
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        return data
+
+
 class TagSerializer(serializers.ModelSerializer):
+    color = Hex2NameColor()
 
     class Meta:
         model = Tag
