5536725ba0449a54a1f37e36393ead912f47120f
Михаил Миноцкий <smoke-m@yandex.com>
2023-07-12 16:15:46 +0300

    aded workflows flake8

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -2,9 +2,9 @@ from io import BytesIO
 
 from django.http import HttpResponse
 from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
 from rest_framework import status
 from rest_framework.response import Response
 
